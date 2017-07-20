67b542ecef8d7328fc59f12eb797249154a6b275
SURAMERICANA\carlvarn <1037636590>
2017-07-20 11:01:09 -0500

    Project Structure Problem description in readme .gitignore

---
+++ src/main/java/com/codewars/MorseCodeDecoder.java
@@ -0,0 +1,10 @@
+package com.codewars;
+
+/**
+ * Created by carlvarn on 20/07/2017.
+ */
+public class MorseCodeDecoder {
+    public static String decode(String morseCode) {
+        return "";
+    }
+}
---
+++ src/test/java/com/codewars/MorseCodeDecoderTest.java
@@ -0,0 +1,15 @@
+package com.codewars;
+
+/**
+ * Created by carlvarn on 20/07/2017.
+ */
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class MorseCodeDecoderTest {
+    @Test
+    public void testExampleFromDescription() {
+        assertThat(MorseCodeDecoder.decode(".... . -.--   .--- ..- -.. ."), is("HEY JUDE"));
+    }
+}
